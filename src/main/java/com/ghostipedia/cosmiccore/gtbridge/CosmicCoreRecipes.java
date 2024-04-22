1bb726b9cc8dfcead8e314200eccc52f2abba556
三好エイラ <eira@miyoshi.app>
2024-04-22 14:20:49 +0200

    Hotfix and slight change on how CoreRecipes is init:ed for future sanity's sake

--- src/main/java/com/ghostipedia/cosmiccore/gtbridge/CosmicCoreRecipes.java
+++ src/main/java/com/ghostipedia/cosmiccore/gtbridge/CosmicCoreRecipes.java
@@ -7,14 +7,20 @@ import net.minecraft.world.level.block.Blocks;
 import java.util.function.Consumer;
 
 import static com.ghostipedia.cosmiccore.gtbridge.CosmicCoreRecipeTypes.SOUL_TESTER_RECIPES;
+import static com.gregtechceu.gtceu.common.data.GTMaterials.Oxygen;
 
 public class CosmicCoreRecipes {
 
 
-    public static void init (Consumer<FinishedRecipe> provider) {
+    public static void init(Consumer<FinishedRecipe> provider) {
+        MultiBlockRecipes(provider);
+    }
+
+
+    private static void MultiBlockRecipes(Consumer<FinishedRecipe> provider) {
         SOUL_TESTER_RECIPES.recipeBuilder("soul_to_dirt")
                 .inputItems(Blocks.DIRT.asItem())
-                .output(SoulRecipeCapability.CAP, 10)
+                .outputFluids(Oxygen.getFluid(1)) //Make sure this is correct
                 .duration(60)
                 .save(provider);
     }
