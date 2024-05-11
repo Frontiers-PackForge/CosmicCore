139100efb7e3c87b4d5220241c03b13afe93c4a1
screret <68943070+screret@users.noreply.github.com>
2024-05-11 16:26:34 +0300

    fix coil materials

--- src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicCoilBlock.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicCoilBlock.java
@@ -11,10 +11,12 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.StringRepresentable;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Supplier;
+
 public class CosmicCoilBlock {
     public enum CoilType implements StringRepresentable, ICoilType {
-        PRISMATIC_TUNGSTENSTEEL("prismatic_tungstensteel", 4500, 4, 2, CosmicMaterials.PrismaticTungstensteel, CosmicCore.id("block/casings/coils/prismatic_tungstensteel")),
-        SHIMMERING_NEUTRONIUM("shimmering_neutronium", 19840, 20, 10, null, CosmicCore.id("block/casings/coils/shimmering_neutronium"));
+        PRISMATIC_TUNGSTENSTEEL("prismatic_tungstensteel", 4500, 3, 4, 2, () -> CosmicMaterials.PrismaticTungstensteel, CosmicCore.id("block/casings/coils/prismatic_tungstensteel")),
+        SHIMMERING_NEUTRONIUM("shimmering_neutronium", 19840, 8, 20, 10, () -> null, CosmicCore.id("block/casings/coils/shimmering_neutronium"));
 
         @NotNull
         @Getter
@@ -22,27 +24,30 @@ public class CosmicCoilBlock {
         //electric blast furnace properties
         @Getter
         private final int coilTemperature;
+        @Getter
+        private final int tier;
         //multi smelter properties
         @Getter
         private final int level;
         @Getter
         private final int energyDiscount;
-        @NotNull @Getter
-        private final Material material;
+        @NotNull
+        private final Supplier<Material> material;
         @NotNull @Getter
         private final ResourceLocation texture;
 
-        CoilType(String name, int coilTemperature, int level, int energyDiscount, Material material, ResourceLocation texture) {
+        CoilType(String name, int coilTemperature, int tier, int level, int energyDiscount, Supplier<Material> material, ResourceLocation texture) {
             this.name = name;
             this.coilTemperature = coilTemperature;
+            this.tier = tier;
             this.level = level;
             this.energyDiscount = energyDiscount;
             this.material = material;
             this.texture = texture;
         }
 
-        public int getTier() {
-            return this.ordinal();
+        public Material getMaterial() {
+            return material.get();
         }
 
         @NotNull
