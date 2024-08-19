3db4aea33b4630b59a190c0d7ea17eedfa7a7d65
Ghostipedia <Ghostipedia@gmail.com>
2024-08-19 01:45:10 -0400

    Updating Cores KubeSchema

--- src/main/java/com/ghostipedia/cosmiccore/integration/kjs/recipe/CosmicCoreRecipeSchema.java
+++ src/main/java/com/ghostipedia/cosmiccore/integration/kjs/recipe/CosmicCoreRecipeSchema.java
@@ -36,7 +36,10 @@ public interface CosmicCoreRecipeSchema {
         }
     }
 
-    RecipeSchema SCHEMA = new RecipeSchema(CosmicRecipeJS.class, CosmicRecipeJS::new, DURATION, DATA, CONDITIONS, ALL_INPUTS, ALL_TICK_INPUTS, ALL_OUTPUTS, ALL_TICK_OUTPUTS, IS_FUEL)
+    RecipeSchema SCHEMA = new RecipeSchema(CosmicRecipeJS.class, CosmicRecipeJS::new, DURATION, DATA, CONDITIONS,
+            ALL_INPUTS, ALL_TICK_INPUTS, ALL_OUTPUTS, ALL_TICK_OUTPUTS,
+            INPUT_CHANCE_LOGICS, OUTPUT_CHANCE_LOGICS, TICK_INPUT_CHANCE_LOGICS, TICK_OUTPUT_CHANCE_LOGICS,
+            IS_FUEL)
             .constructor((recipe, schemaType, keys, from) -> recipe.id(from.getValue(recipe, ID)), ID)
             .constructor(DURATION, CONDITIONS, ALL_INPUTS, ALL_OUTPUTS, ALL_TICK_INPUTS, ALL_TICK_OUTPUTS);
 
