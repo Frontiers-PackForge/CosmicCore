b798f7a8a1dd648edbec1d75ee4fceea2fef817f
screret <68943070+screret@users.noreply.github.com>
2024-08-23 08:35:36 +0300

    fix server crash (#28)

--- src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicBlockEntities.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicBlockEntities.java
@@ -1,21 +1,19 @@
 package com.ghostipedia.cosmiccore.common.data;
 
+import com.ghostipedia.cosmiccore.client.renderer.block.NebulaeCoilRenderer;
 import com.ghostipedia.cosmiccore.common.blockentity.CosmicCoilBlockEntity;
-import com.gregtechceu.gtceu.common.blockentity.CableBlockEntity;
-import com.gregtechceu.gtceu.common.data.GTBlocks;
-import com.lowdragmc.lowdraglib.client.renderer.ATESRRendererProvider;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
-import com.tterrag.registrate.util.entry.BlockEntry;
 
 import static com.gregtechceu.gtceu.common.registry.GTRegistration.REGISTRATE;
 
+@SuppressWarnings("Convert2MethodRef")
 public class CosmicBlockEntities {
+
     public static final BlockEntityEntry<CosmicCoilBlockEntity> CAUSAL_FABRIC_COIL_BLOCK_ENTITY = REGISTRATE
             .blockEntity("causal_fabric_coil", CosmicCoilBlockEntity::new)
-            .renderer(() -> ctx -> new ATESRRendererProvider<>())
+            .renderer(() -> NebulaeCoilRenderer.createBlockEntityRenderer())
             .validBlocks(CosmicBlocks.COIL_CAUSAL_FABRIC)
             .register();
-
 
     public static void init() {
 
