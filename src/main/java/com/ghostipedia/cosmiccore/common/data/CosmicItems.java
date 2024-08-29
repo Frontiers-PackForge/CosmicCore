df91cf3a4a90820953e28fc06d2485861fd85747
Ghostipedia <Ghostipedia@gmail.com>
2024-08-29 06:31:25 -0400

    burp

--- src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicItems.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicItems.java
@@ -28,10 +28,11 @@ import static wayoftime.bloodmagic.common.item.BloodMagicItems.BLOOD_ORBS;
 
 
 @SuppressWarnings("Convert2MethodRef")
-public class CosmicItems  {
+public class CosmicItems {
     public static final BloodOrbRegistryObject<BloodOrb> ORB_ASCENDANT;
     public static final BloodOrbRegistryObject<BloodOrb> ORB_VOIDSENT;
     public static final BloodOrbRegistryObject<BloodOrb> ORB_SOVEREIGN;
+
     static {
         CosmicRegistration.REGISTRATE.creativeModeTab(() -> CosmicCreativeModeTabs.COSMIC_CORE);
         ORB_ASCENDANT = BLOOD_ORBS.register("ascendantbloodorb", () -> {
@@ -44,18 +45,19 @@ public class CosmicItems  {
             return new BloodOrb(new ResourceLocation("bloodmagic", "sovereignbloodorb"), 5, 100000000, 10000);
         });
     }
+
     public static final CosmicBloodOrbDeferredRegister COSMIC_BLOOD_ORBS = new CosmicBloodOrbDeferredRegister("cosmiccore");
 
-//Literally Random shit
+    //Literally Random shit
     public static final ItemEntry<ComponentItem> DONK = REGISTRATE.item("donk", ComponentItem::create)
             .lang("Donk")
             .properties(p -> p.stacksTo(16))
-        .tag()
+            .tag()
             .defaultModel()
             .register();
     public static final ItemEntry<ComponentItem> DILUMIXAL_NAQ_DOPED_BOULE = REGISTRATE.item("dilumixal_naquadah_doped_silicon_boule", ComponentItem::create)
             .lang("DiLumixal Naquadah Doped Silicon Boule")
-            .properties(p -> p.stacksTo(64) )
+            .properties(p -> p.stacksTo(64))
             .tag()
             .defaultModel()
             .register();
@@ -129,7 +131,7 @@ public class CosmicItems  {
             .defaultModel()
             .register();
 
-//    public static ItemEntry<SpaceArmorComponentItem> SPACE_NANO_CHESTPLATE = REGISTRATE.item("space_nanomuscle_chestplate", (p) -> new SpaceArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.CHESTPLATE, 5000, p)
+    //    public static ItemEntry<SpaceArmorComponentItem> SPACE_NANO_CHESTPLATE = REGISTRATE.item("space_nanomuscle_chestplate", (p) -> new SpaceArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.CHESTPLATE, 5000, p)
 //                    .setArmorLogic(new NanoMuscleSpaceSuite(ArmorItem.Type.CHESTPLATE, 512,
 //                            6_400_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierNanoSuit - 3)),
 //                            ConfigHolder.INSTANCE.tools.voltageTierNanoSuit)))
@@ -137,9 +139,32 @@ public class CosmicItems  {
 //            .lang("NanoMuscle™ Space Suite Chestplate")
 //            .properties(p -> p.rarity(Rarity.RARE))
 //            .register();
+    public static final ItemEntry<ComponentItem> THERMAL_CHAIN_AGENT = REGISTRATE.item("thermal_chain_agent", ComponentItem::create)
+            .lang("Thermal Chain Agent")
+            .properties(p -> p.stacksTo(64))
+            .defaultModel()
+            .register();
 
-
-
+    public static final ItemEntry<ComponentItem> VOMAPLAST = REGISTRATE.item("vomaplast", ComponentItem::create)
+            .lang("Vomaplast")
+            .properties(p -> p.stacksTo(64))
+            .defaultModel()
+            .register();
+    public static final ItemEntry<ComponentItem> ECTOPHASM = REGISTRATE.item("ectophasm", ComponentItem::create)
+            .lang("Ectophasm")
+            .properties(p -> p.stacksTo(64))
+            .defaultModel()
+            .register();
+    public static final ItemEntry<ComponentItem> DEMONIC_DESIRE = REGISTRATE.item("demonic_desire", ComponentItem::create)
+            .lang("Deomic Desire")
+            .properties(p -> p.stacksTo(64))
+            .defaultModel()
+            .register();
+    public static final ItemEntry<ComponentItem> WEAKENED_SOUL = REGISTRATE.item("weakened_soul", ComponentItem::create)
+            .lang("Weakened Soul")
+            .properties(p -> p.stacksTo(64))
+            .defaultModel()
+            .register();
 
 
     public static final ItemEntry<ComponentItem> WAXED_LEATHER = REGISTRATE.item("waxed_leather", ComponentItem::create)
@@ -174,8 +199,8 @@ public class CosmicItems  {
             .defaultModel()
             .register();
 
- //New Circuits
- //Echo (ZPM-UEV)
+    //New Circuits
+    //Echo (ZPM-UEV)
     public static final ItemEntry<ComponentItem> SONAR_PROCESSOR = REGISTRATE.item("echo_processor", ComponentItem::create)
             .lang("Echo Processor")
             .properties(p -> p.stacksTo(64))
@@ -196,7 +221,7 @@ public class CosmicItems  {
             .properties(p -> p.stacksTo(64))
             .defaultModel()
             .register();
-//Optical (UV-UIV)
+    //Optical (UV-UIV)
     public static final ItemEntry<ComponentItem> OPTICAL_PROCESSOR = REGISTRATE.item("optical_processor", ComponentItem::create)
             .lang("Optical Processor")
             .properties(p -> p.stacksTo(64))
@@ -217,7 +242,7 @@ public class CosmicItems  {
             .properties(p -> p.stacksTo(64))
             .defaultModel()
             .register();
-//Cosmic (UHV-UXV)
+    //Cosmic (UHV-UXV)
     public static final ItemEntry<ComponentItem> COSMIC_PROCESSOR = REGISTRATE.item("cosmic_processor", ComponentItem::create)
             .lang("Cosmic Processor")
             .properties(p -> p.stacksTo(64))
@@ -238,7 +263,7 @@ public class CosmicItems  {
             .properties(p -> p.stacksTo(64))
             .defaultModel()
             .register();
-//Psionic Circuit (UEV-OPV)
+    //Psionic Circuit (UEV-OPV)
     public static final ItemEntry<ComponentItem> PSIONIC_PROCESSOR = REGISTRATE.item("psionic_processor", ComponentItem::create)
             .lang("Psionic Processor")
             .properties(p -> p.stacksTo(64))
@@ -283,33 +308,33 @@ public class CosmicItems  {
 
     // Space Suite
     public static ItemEntry<SpaceArmorComponentItem> SPACE_NANO_CHESTPLATE = REGISTRATE.item("space_nanomuscle_chestplate", (p) -> new SpaceArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.CHESTPLATE, 5000, p)
-            .setArmorLogic(new NanoMuscleSpaceSuite(ArmorItem.Type.CHESTPLATE, 512,
-                    6_400_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierNanoSuit - 3)),
-                    ConfigHolder.INSTANCE.tools.voltageTierNanoSuit)))
+                    .setArmorLogic(new NanoMuscleSpaceSuite(ArmorItem.Type.CHESTPLATE, 512,
+                            6_400_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierNanoSuit - 3)),
+                            ConfigHolder.INSTANCE.tools.voltageTierNanoSuit)))
             .tag(CosmicItemTags.NANOMUSCLE_SPACE_SUITE, ModItemTags.SPACE_SUITS, ModItemTags.FREEZE_RESISTANT_ARMOR, ModItemTags.HEAT_RESISTANT_ARMOR)
             .lang("NanoMuscle™ Space Suite Chestplate")
             .properties(p -> p.rarity(Rarity.RARE))
             .register();
     public static ItemEntry<SpaceArmorComponentItem> ADVANCED_SPACE_NANO_CHESTPLATE = REGISTRATE.item("space_advanced_nanomuscle_chestplate", (p) -> new SpaceArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.CHESTPLATE, 10000, p)
-            .setArmorLogic(new AdvancedNanoMuscleSpaceSuite(512,
-                    12_800_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierAdvNanoSuit - 3)),
-                    ConfigHolder.INSTANCE.tools.voltageTierAdvNanoSuit)))
+                    .setArmorLogic(new AdvancedNanoMuscleSpaceSuite(512,
+                            12_800_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierAdvNanoSuit - 3)),
+                            ConfigHolder.INSTANCE.tools.voltageTierAdvNanoSuit)))
             .tag(CosmicItemTags.NANOMUSCLE_SPACE_SUITE, ModItemTags.SPACE_SUITS, ModItemTags.FREEZE_RESISTANT_ARMOR, ModItemTags.HEAT_RESISTANT_ARMOR)
             .lang("Advanced NanoMuscle™ Space Suite Chestplate")
             .properties(p -> p.rarity(Rarity.EPIC))
             .register();
     public static ItemEntry<SpaceArmorComponentItem> SPACE_QUARK_CHESTPLATE = REGISTRATE.item("space_quarktech_chestplate", (p) -> new SpaceArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.CHESTPLATE, 20000, p)
-            .setArmorLogic(new QuarkTechSpaceSuite(ArmorItem.Type.CHESTPLATE, 8192,
-                    100_000_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierQuarkTech - 5)),
-                    ConfigHolder.INSTANCE.tools.voltageTierNanoSuit)))
+                    .setArmorLogic(new QuarkTechSpaceSuite(ArmorItem.Type.CHESTPLATE, 8192,
+                            100_000_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierQuarkTech - 5)),
+                            ConfigHolder.INSTANCE.tools.voltageTierNanoSuit)))
             .tag(CosmicItemTags.QUARKTECH_SPACE_SUITE, ModItemTags.SPACE_SUITS, ModItemTags.FREEZE_RESISTANT_ARMOR, ModItemTags.HEAT_RESISTANT_ARMOR)
             .lang("QuarkTech™ Space Suite Chestplate")
             .properties(p -> p.rarity(Rarity.RARE))
             .register();
     public static ItemEntry<SpaceArmorComponentItem> ADVANCED_SPACE_QUARK_CHESTPLATE = REGISTRATE.item("space_advanced_quarktech_chestplate", (p) -> new SpaceArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.CHESTPLATE, 50000, p)
-            .setArmorLogic(new AdvancedQuarkTechSpaceSuite(8192,
-                    1_000_000_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierAdvQuarkTech - 6)),
-                    ConfigHolder.INSTANCE.tools.voltageTierAdvNanoSuit)))
+                    .setArmorLogic(new AdvancedQuarkTechSpaceSuite(8192,
+                            1_000_000_000L * (long) Math.max(1, Math.pow(4, ConfigHolder.INSTANCE.tools.voltageTierAdvQuarkTech - 6)),
+                            ConfigHolder.INSTANCE.tools.voltageTierAdvNanoSuit)))
             .tag(CosmicItemTags.QUARKTECH_SPACE_SUITE, ModItemTags.SPACE_SUITS, ModItemTags.FREEZE_RESISTANT_ARMOR, ModItemTags.HEAT_RESISTANT_ARMOR)
             .lang("Advanced QuarkTech™ Space Suite Chestplate")
             .properties(p -> p.rarity(Rarity.EPIC))
@@ -364,6 +389,7 @@ public class CosmicItems  {
             .properties(p -> p.rarity(Rarity.EPIC))
             .tag(CustomTags.PPE_ARMOR)
             .register();
+
     public static <T extends ComponentItem> NonNullConsumer<T> attach(IItemComponent... components) {
         return item -> item.attachComponents(components);
     }
