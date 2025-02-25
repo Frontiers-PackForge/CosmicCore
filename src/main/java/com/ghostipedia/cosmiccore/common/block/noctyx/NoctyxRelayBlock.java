76e88dd323b5ce3b78697a57b20b5d70e9629a3e
omergunr100 <omer.er10@gmail.com>
2025-02-25 04:18:35 +0200

    relay and connector block start

---
+++ src/main/java/com/ghostipedia/cosmiccore/common/block/noctyx/NoctyxRelayBlock.java
@@ -0,0 +1,29 @@
+package com.ghostipedia.cosmiccore.common.block.noctyx;
+
+import net.minecraft.core.Direction;
+
+import org.jetbrains.annotations.NotNull;
+import org.joml.Vector3f;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class NoctyxRelayBlock extends NoctyxBlock {
+
+    protected static final Map<Direction, Vector3f> offsetMap = new EnumMap<>(Direction.class);
+    static {
+        // todo: fill out actual values according to model (this is the offset to the laser target)
+        for (var direction : Direction.values()) {
+            offsetMap.put(direction, defaultOffset);
+        }
+    }
+
+    public NoctyxRelayBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public @NotNull Vector3f getLaserOffset(Direction attachedSide) {
+        return offsetMap.get(attachedSide);
+    }
+}
