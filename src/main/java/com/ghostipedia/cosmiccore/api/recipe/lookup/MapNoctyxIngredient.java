c78c69bb1bc4848d90c28ca632dbb9ab9fe59bba
omergunr100 <omer.er10@gmail.com>
2025-02-28 13:10:33 +0200

    noctyx ingredient

---
+++ src/main/java/com/ghostipedia/cosmiccore/api/recipe/lookup/MapNoctyxIngredient.java
@@ -0,0 +1,43 @@
+package com.ghostipedia.cosmiccore.api.recipe.lookup;
+
+import com.ghostipedia.cosmiccore.api.noctyx.NoctyxStack;
+
+import com.gregtechceu.gtceu.api.recipe.lookup.AbstractMapIngredient;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+public class MapNoctyxIngredient extends AbstractMapIngredient {
+
+    public final NoctyxStack[] stacks;
+
+    @Override
+    protected int hash() {
+        return Arrays.hashCode(stacks);
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof MapNoctyxIngredient that)) return false;
+        if (!super.equals(object)) return false;
+
+        return Arrays.equals(stacks, that.stacks);
+    }
+
+    @Override
+    public String toString() {
+        var builder = new StringBuilder();
+        builder.append("MapNoctyxIngredient{ NoctyxStack[] stacks=");
+        for (int i = 0; i < stacks.length; i++) {
+            builder.append(stacks[i].toString());
+            if (i < stacks.length - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append('}');
+        return builder.toString();
+    }
+}
