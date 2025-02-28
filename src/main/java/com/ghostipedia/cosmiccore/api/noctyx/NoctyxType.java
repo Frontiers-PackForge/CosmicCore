15961cb14b1d7f224726403eecc6d2c21595780b
omergunr100 <omer.er10@gmail.com>
2025-02-28 13:06:02 +0200

    define noctyx type and codec

--- src/main/java/com/ghostipedia/cosmiccore/api/noctyx/NoctyxType.java
+++ src/main/java/com/ghostipedia/cosmiccore/api/noctyx/NoctyxType.java
@@ -1,24 +1,128 @@
 package com.ghostipedia.cosmiccore.api.noctyx;
 
-import static net.minecraft.util.FastColor.ARGB32.color;
+import com.lowdragmc.lowdraglib.syncdata.ITagSerializable;
+import com.lowdragmc.lowdraglib.utils.ColorUtils;
 
-public enum NoctyxType {
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.util.GsonHelper;
 
-    ALL("ALL", color(255, 255, 255, 255)),
-    TYPE_0("0", color(255, 255, 0, 255)),
-    TYPE_1("1", color(255, 204, 204, 255)),
-    TYPE_2("2", color(255, 127, 221, 234)),
-    TYPE_3("3", color(255, 160, 255, 219)),
-    TYPE_4("4", color(255, 255, 186, 105)),
-    TYPE_5("5", color(255, 255, 111, 105)),
-    TYPE_6("6", color(255, 43, 50, 86)),
-    TYPE_7("7", color(255, 119, 22, 70));
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
-    public final String name;
-    public final int color;
+// todo: remove interface abstraction that doesn't help
+@NoArgsConstructor
+@AllArgsConstructor
+@MethodsReturnNonnullByDefault
+public class NoctyxType implements ITagSerializable<CompoundTag> {
 
-    NoctyxType(String name, int color) {
-        this.name = name;
-        this.color = color;
+    @Accessors(fluent = true)
+    @Getter
+    protected String lang;
+    @Accessors(fluent = true)
+    @Getter
+    protected float alpha;
+    @Accessors(fluent = true)
+    @Getter
+    protected float red;
+    @Accessors(fluent = true)
+    @Getter
+    protected float green;
+    @Accessors(fluent = true)
+    @Getter
+    protected float blue;
+
+    public NoctyxType(CompoundTag tag) {
+        deserializeNBT(tag);
     }
+
+    public NoctyxType(JsonObject json) {
+        fromJson(json);
+    }
+
+    public MutableComponent getDisplayName() {
+        return Component.translatable(lang)
+                .withStyle(style -> style.withColor(ColorUtils.color(alpha, red, green, blue)));
+    }
+
+    public String name() {
+        return getDisplayName().getString();
+    }
+
+    @Override
+    public CompoundTag serializeNBT() {
+        var tag = new CompoundTag();
+        tag.putString("lang", lang);
+        tag.putFloat("a", alpha);
+        tag.putFloat("r", red);
+        tag.putFloat("g", green);
+        tag.putFloat("b", blue);
+        return tag;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundTag tag) {
+        lang = tag.getString("lang");
+        alpha = tag.getFloat("a");
+        red = tag.getFloat("r");
+        green = tag.getFloat("g");
+        blue = tag.getFloat("b");
+    }
+
+    public JsonElement toJson() {
+        var json = new JsonObject();
+        json.addProperty("lang", lang);
+        json.addProperty("a", alpha);
+        json.addProperty("r", red);
+        json.addProperty("g", green);
+        json.addProperty("b", blue);
+        return json;
+    }
+
+    public void fromJson(JsonElement json) {
+        if (!json.isJsonObject()) {
+            return;
+        }
+        var jsonObject = GsonHelper.convertToJsonObject(json, "type");
+        this.lang = jsonObject.getAsJsonObject("lang").getAsString();
+        this.alpha = jsonObject.getAsJsonObject("a").getAsFloat();
+        this.red = jsonObject.getAsJsonObject("r").getAsFloat();
+        this.green = jsonObject.getAsJsonObject("g").getAsFloat();
+        this.blue = jsonObject.getAsJsonObject("b").getAsFloat();
+    }
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof NoctyxType that)) return false;
+
+        return Float.compare(alpha, that.alpha) == 0 && Float.compare(red, that.red) == 0 &&
+                Float.compare(green, that.green) == 0 && Float.compare(blue, that.blue) == 0 && lang.equals(that.lang);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lang.hashCode();
+        result = 31 * result + Float.hashCode(alpha);
+        result = 31 * result + Float.hashCode(red);
+        result = 31 * result + Float.hashCode(green);
+        result = 31 * result + Float.hashCode(blue);
+        return result;
+    }
+
+    public static final Codec<NoctyxType> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.STRING.fieldOf("lang").forGetter(NoctyxType::lang),
+            Codec.FLOAT.fieldOf("alpha").forGetter(NoctyxType::alpha),
+            Codec.FLOAT.fieldOf("red").forGetter(NoctyxType::red),
+            Codec.FLOAT.fieldOf("green").forGetter(NoctyxType::green),
+            Codec.FLOAT.fieldOf("blue").forGetter(NoctyxType::blue))
+            .apply(instance, NoctyxType::new));
 }
