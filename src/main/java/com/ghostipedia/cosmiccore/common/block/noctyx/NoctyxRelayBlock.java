53f94bd51cde5a3e7d5918871469503fd188d57c
omergunr100 <omer.er10@gmail.com>
2025-02-28 13:13:30 +0200

    will need to partially implement a custom pipenet for it to work, sadge

--- src/main/java/com/ghostipedia/cosmiccore/common/block/noctyx/NoctyxRelayBlock.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/block/noctyx/NoctyxRelayBlock.java
@@ -1,14 +1,28 @@
 package com.ghostipedia.cosmiccore.common.block.noctyx;
 
+import com.ghostipedia.cosmiccore.common.blockentity.NoctyxBlockEntity;
+import com.ghostipedia.cosmiccore.common.data.CosmicBlockEntities;
+
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3f;
 
 import java.util.EnumMap;
 import java.util.Map;
 
-public class NoctyxRelayBlock extends NoctyxBlock {
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class NoctyxRelayBlock extends NoctyxBlock implements EntityBlock {
 
     protected static final Map<Direction, Vector3f> offsetMap = new EnumMap<>(Direction.class);
     static {
@@ -25,5 +39,17 @@ public class NoctyxRelayBlock extends NoctyxBlock {
     @Override
     public @NotNull Vector3f getLaserOffset(Direction attachedSide) {
         return offsetMap.get(attachedSide);
+    }
+
+    @Override
+    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return CosmicBlockEntities.NOCTYX_BLOCK_ENTITY.create(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state,
+                                                                  BlockEntityType<T> blockEntityType) {
+        return (l, p, s, blockEntity) -> ((NoctyxBlockEntity) blockEntity).serverTick();
     }
 }
--- src/main/java/com/ghostipedia/cosmiccore/common/blockentity/NoctyxBlockEntity.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/blockentity/NoctyxBlockEntity.java
@@ -2,8 +2,6 @@ package com.ghostipedia.cosmiccore.common.blockentity;
 
 import com.ghostipedia.cosmiccore.api.noctyx.NoctyxType;
 
-import com.gregtechceu.gtceu.api.capability.recipe.IO;
-
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -13,9 +11,6 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,15 +26,12 @@ public class NoctyxBlockEntity extends BlockEntity {
     @Getter
     protected List<BlockPos> neighbors;
     @Getter
-    protected Table<NoctyxType, IO, List<BlockPos>> connections;
-    @Getter
     @Setter
     protected NoctyxType ownType;
 
     public NoctyxBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
         super(type, pos, blockState);
         neighbors = new ArrayList<>();
-        connections = HashBasedTable.create();
     }
 
     public Direction getUpwardFacing() {
@@ -49,14 +41,19 @@ public class NoctyxBlockEntity extends BlockEntity {
         return Direction.UP;
     }
 
+    public void serverTick() {
+        // todo: implement network things
+    }
+
     // saving and loading
 
     @Override
     public void load(CompoundTag tag) {
         super.load(tag);
         if (tag.contains("ownType")) {
-            var ownType = tag.getInt("ownType");
-            this.ownType = NoctyxType.values()[ownType];
+            var ownTypeTag = tag.getCompound("ownType");
+            this.ownType = new NoctyxType();
+            this.ownType.deserializeNBT(ownTypeTag);
         }
         if (tag.contains("neighbors")) {
             var neighbors = tag.getIntArray("neighbors");
@@ -64,61 +61,16 @@ public class NoctyxBlockEntity extends BlockEntity {
                 this.neighbors.add(new BlockPos(neighbors[i], neighbors[i + 1], neighbors[i + 2]));
             }
         }
-        if (tag.contains("connections")) {
-            var connections = tag.getCompound("connections");
-            var rows = connections.getInt("rows");
-            for (int i = 0; i < rows; i++) {
-                var rowTag = connections.getCompound("row" + i);
-                var type = NoctyxType.values()[rowTag.getInt("type")];
-                for (var io : IO.values()) {
-                    var key = String.valueOf(io.ordinal());
-                    if (!rowTag.contains(key)) {
-                        continue;
-                    }
-                    var positions = rowTag.getIntArray(key);
-                    var posList = new ObjectArrayList<BlockPos>(positions.length / 3);
-                    this.connections.put(type, io, posList);
-                    for (int j = 0; positions.length - j >= 3; j += 3) {
-                        posList.add(new BlockPos(positions[j], positions[j + 1], positions[j + 2]));
-                    }
-                }
-            }
-        }
     }
 
     @Override
     protected void saveAdditional(CompoundTag tag) {
         super.saveAdditional(tag);
-        tag.putInt("ownType", ownType.ordinal());
+        tag.put("ownType", ownType.serializeNBT());
         tag.putIntArray("neighbors", neighbors.stream().mapMultiToInt((pos, c) -> {
             c.accept(pos.getX());
             c.accept(pos.getY());
             c.accept(pos.getZ());
         }).toArray());
-        var connections = new CompoundTag();
-        tag.put("connections", connections);
-        var typeIter = this.connections.rowKeySet().iterator();
-        var ind = 0;
-        while (typeIter.hasNext()) {
-            var rowTag = new CompoundTag();
-            connections.put("row" + ind, rowTag);
-
-            var key = typeIter.next();
-            rowTag.putInt("type", key.ordinal());
-            var row = this.connections.row(key);
-            for (var io : IO.values()) {
-                var positions = row.get(io);
-                if (positions == null) {
-                    continue;
-                }
-                rowTag.putIntArray(String.valueOf(io.ordinal()), positions.stream().mapMultiToInt((pos, c) -> {
-                    c.accept(pos.getX());
-                    c.accept(pos.getY());
-                    c.accept(pos.getZ());
-                }).toArray());
-            }
-            ind++;
-        }
-        connections.putInt("rows", ind);
     }
 }
