516a3fd4095fa6a632792313afbdad0faada0452
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2025-03-06 23:43:43 -0500

    Remove loom and use forge gradle

--- src/main/java/com/ghostipedia/cosmiccore/mixin/GTRecipeModifiersMixin.java
+++ src/main/java/com/ghostipedia/cosmiccore/mixin/GTRecipeModifiersMixin.java
@@ -8,8 +8,8 @@ import com.gregtechceu.gtceu.api.machine.multiblock.CoilWorkableElectricMultiblo
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 
-import dev.architectury.patchedmixin.staticmixin.spongepowered.asm.mixin.injection.At;
-import dev.architectury.patchedmixin.staticmixin.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
