a911d87486a7d9098cdd2a1f4fa43811a380e7a5
Ghostipedia <Ghostipedia@gmail.com>
2025-03-08 04:48:18 -0500

    Spotless

--- src/main/java/com/ghostipedia/cosmiccore/common/data/lang/CosmicLangHandler.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/lang/CosmicLangHandler.java
@@ -154,8 +154,8 @@ public class CosmicLangHandler {
                 "§eTears of the Universe§f: §c2x §fEU total @ 250mb/hr");
 
         replace(provider, "cosmiccore.universal.boosting_agents.0",
-                "§aCan consume various boosters to increase EU/t multiplier")
-        ;replace(provider, "cosmiccore.errors.bad_fuel",
+                "§aCan consume various boosters to increase EU/t multiplier");
+        replace(provider, "cosmiccore.errors.bad_fuel",
                 "§aInsufficient Fuel Quality! \n Fuel Output Must be >720 EU total per unit");
         replace(provider, "cosmiccore.universal.boosting_agents.1", "§6Oxygen §ffor §a3x §fEu/t @ §b20mb/s ");
         replace(provider, "cosmiccore.universal.boosting_agents.2", "§6Liquid Oxygen §ffor §a6x §fEu/t @ §b80mb/s ");
