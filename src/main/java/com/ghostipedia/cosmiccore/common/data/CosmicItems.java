44dc4632b2a7b0edbb6f8a8aaadb018cb9556851
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-03-09 08:12:12 +0000

    Spot dn

--- src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicItems.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/CosmicItems.java
@@ -377,12 +377,12 @@ public class CosmicItems {
             .defaultModel()
             .register();
 
-//    public static final ItemEntry<ComponentItem> WRAPPED_S = REGISTRATE
-//            .item("blackstone_pustule", ComponentItem::create)
-//            .lang("Blackstone Pustule")
-//            .properties(p -> p.stacksTo(64))
-//            .defaultModel()
-//            .register();
+    // public static final ItemEntry<ComponentItem> WRAPPED_S = REGISTRATE
+    // .item("blackstone_pustule", ComponentItem::create)
+    // .lang("Blackstone Pustule")
+    // .properties(p -> p.stacksTo(64))
+    // .defaultModel()
+    // .register();
 
     // New Circuits
     // Echo (ZPM-UEV)
