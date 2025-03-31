4a774aa2500db828b9419fc2db4c7472650c73b7
Furuchi <matheuscarniellogms.mf@gmail.com>
2025-03-31 09:48:15 -0300

    Revert "Addition of trinavine material-set"
    This reverts commit d881a729638936acc0e167beb8aeadf994ad7ae9.

--- src/main/java/com/ghostipedia/cosmiccore/common/data/materials/CosmicMaterialSet.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/materials/CosmicMaterialSet.java
@@ -9,7 +9,6 @@ public class CosmicMaterialSet {
     public static final MaterialIconSet INFINITY = new MaterialIconSet("infinity", SHINY);
     public static final MaterialIconSet CHRONON = new MaterialIconSet("chronon", SHINY);
     public static final MaterialIconSet NEUTRONITE = new MaterialIconSet("neutronite", SHINY);
-    public static final MaterialIconSet TRINAVINE = new MaterialIconSet("trinavine", SHINY);
 
     public static void init() {}
 }
