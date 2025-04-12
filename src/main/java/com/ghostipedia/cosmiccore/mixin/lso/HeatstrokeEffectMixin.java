196e1a25d5e4995d49435d89fe3ce29390f90802
Ghostipedia <46772882+Ghostipedia@users.noreply.github.com>
2025-04-12 10:16:21 -0400

    Make Damage Scale Over Time for HeatStroke/Frostbite (#66)
    * this should do
    
    * it did not do.
    
    This reverts commit 3907f59e947bae5d73d2e4399576fad46cec8f4d.
    
    * this should do what was actually requested.
    also the effect mixins should remap because they're targeting vanilla methods.
    
    * how did I mess this up :iCANT:
    
    * Tweaks to Timing and Damage, Ty Mint for the help <3
    
    * Spotless and Merge Conflicts
    
    ---------
    
    Co-authored-by: screret <68943070+screret@users.noreply.github.com>

---
+++ src/main/java/com/ghostipedia/cosmiccore/mixin/lso/HeatstrokeEffectMixin.java
@@ -0,0 +1,33 @@
+package com.ghostipedia.cosmiccore.mixin.lso;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import sfiomn.legendarysurvivaloverhaul.common.effects.HeatStrokeEffect;
+
+@Mixin(value = HeatStrokeEffect.class)
+public abstract class HeatstrokeEffectMixin extends MobEffect {
+
+    protected HeatstrokeEffectMixin(MobEffectCategory category, int color) {
+        super(MobEffectCategory.HARMFUL, 16756041);
+    }
+
+    @ModifyArg(method = "applyEffectTick",
+               at = @At(
+                        value = "INVOKE",
+                        target = "Lnet/minecraft/world/entity/player/Player;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"),
+               index = 1)
+    private float cosmiccore$changeDamageAmount(float amount, @Local(argsOnly = true) int amplifier) {
+        return amount + amplifier;
+    }
+
+    @ModifyVariable(method = "isDurationEffectTick", at = @At(value = "STORE"), ordinal = 2, name = "time")
+    private int cosmiccore$modifyDamageIncrement(int time) {
+        return 50;
+    }
+}
--- src/main/java/com/ghostipedia/cosmiccore/mixin/lso/TemperatureCapabilityMixin.java
+++ src/main/java/com/ghostipedia/cosmiccore/mixin/lso/TemperatureCapabilityMixin.java
@@ -1,49 +1,87 @@
 package com.ghostipedia.cosmiccore.mixin.lso;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import sfiomn.legendarysurvivaloverhaul.api.temperature.ITemperatureCapability;
 import sfiomn.legendarysurvivaloverhaul.api.temperature.TemperatureEnum;
-import sfiomn.legendarysurvivaloverhaul.api.thirst.ThirstUtil;
 import sfiomn.legendarysurvivaloverhaul.common.capabilities.temperature.TemperatureCapability;
-import sfiomn.legendarysurvivaloverhaul.common.effects.FrostbiteEffect;
-import sfiomn.legendarysurvivaloverhaul.common.effects.HeatStrokeEffect;
-import sfiomn.legendarysurvivaloverhaul.config.Config;
-import sfiomn.legendarysurvivaloverhaul.registry.MobEffectRegistry;
 
 @Mixin(value = TemperatureCapability.class, remap = false)
 public abstract class TemperatureCapabilityMixin implements ITemperatureCapability {
 
-    /**
-     * @author Ghostipedia
-     * @reason digging my stupid hands into this so i can reset temp values
-     */
-    @Overwrite
-    private void applyDangerousEffects(Player player, TemperatureEnum tempEnum) {
-        if (Config.Baked.dangerousHeatTemperature && ThirstUtil.isThirstActive(player) &&
-                tempEnum == TemperatureEnum.HEAT_STROKE) {
-            if (TemperatureEnum.HEAT_STROKE.getMiddle() <= getTemperatureLevel() &&
-                    !HeatStrokeEffect.playerIsImmuneToHeat(player)) {
-                // Apply hyperthermia effect
-                if (!player.hasEffect(MobEffectRegistry.HEAT_STROKE.get()))
-                    player.addEffect(new MobEffectInstance(MobEffectRegistry.HEAT_STROKE.get(), -1, 0, false, true));
-                return;
-            }
-        } else if (Config.Baked.dangerousColdTemperature && tempEnum == TemperatureEnum.FROSTBITE) {
-            if (TemperatureEnum.FROSTBITE.getMiddle() >= getTemperatureLevel() &&
-                    !FrostbiteEffect.playerIsImmuneToFrost(player)) {
-                // Apply hypothermia effect
-                if (!player.hasEffect(MobEffectRegistry.FROSTBITE.get()))
-                    player.addEffect(new MobEffectInstance(MobEffectRegistry.FROSTBITE.get(), -1, 0, false, true));
-                return;
-            }
+    @Unique
+    private int cosmiccore$badTimeTimer = 0;
+
+    @Inject(method = "applyDangerousEffects", at = @At("HEAD"))
+    private void cosmiccore$trackBadTime(Player player, TemperatureEnum tempEnum, CallbackInfo ci) {
+        // make the player's time alive worse if they're too hot or colds for too long
+        cosmiccore$badTimeTimer += switch (tempEnum) {
+            case HEAT_STROKE, FROSTBITE -> 2; // Bad Timer Increases while under the effect of something deadly
+            case HOT, COLD -> 0; // Store whatever their previous negative effect was, hold it until they've entered a
+                                 // survivable temp range.
+            case NORMAL -> -4; // Survivable Environment, decrease bad Timer to avoid getting punched with really nasty
+                               // damage
+        };
+        cosmiccore$badTimeTimer = Math.max(cosmiccore$badTimeTimer, 0);
+    }
+
+    @ModifyExpressionValue(method = "applyDangerousEffects",
+                           at = @At(value = "NEW", target = "net/minecraft/world/effect/MobEffectInstance"))
+    private MobEffectInstance cosmiccore$modifyDangerousEffects(MobEffectInstance effect, Player player,
+                                                                TemperatureEnum tempEnum) {
+        // change this to give more/less time before the inevitable
+        final int MAX_FREE_TIME_SECONDS = 60;
+        int extra = cosmiccore$badTimeTimer - MAX_FREE_TIME_SECONDS;
+        if (extra <= 0) {
+            return effect;
         }
-        if (player.hasEffect(MobEffectRegistry.HEAT_STROKE.get()))
-            player.removeEffect(MobEffectRegistry.HEAT_STROKE.get());
-        if (player.hasEffect(MobEffectRegistry.FROSTBITE.get()))
-            player.removeEffect(MobEffectRegistry.FROSTBITE.get());
+        // add +1 level of effect for every 10 seconds over the damage threshold
+        int amplifier = extra / 10;
+        return new MobEffectInstance(effect.getEffect(), effect.getDuration(), effect.getAmplifier() + amplifier);
+    }
+
+    // this target is funny :3
+    // Don't think about it too hard, ok?
+    @ModifyExpressionValue(method = "applyDangerousEffects",
+                           at = @At(
+                                    value = "INVOKE",
+                                    target = "Lnet/minecraft/world/entity/player/Player;hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z"),
+                           slice = @Slice(
+                                          from = @At("HEAD"),
+                                          to = @At(value = "RETURN", ordinal = 1)))
+    // This method patches the effect to be reapplied every time applyDangerousEffects is called.
+    // It works fine, because MC doesn't error when existing effects are reapplied and instead modifies them
+    // to match the new effect.
+    private boolean cosmiccore$alwaysApplyEffect(boolean original) {
+        // returns false because it's inverted in the if statement. And I can't change that.
+        // see https://github.com/SpongePowered/Mixin/issues/365#issuecomment-539464542 for an explanation.
+        return false;
+    }
+
+    // someone should PR this. Not me though :)
+    @ModifyConstant(method = "writeNBT", constant = @Constant(stringValue = "ticktimer"))
+    private String cosmiccore$fixSaveDataBug(String original) {
+        return "tickTimer";
+    }
+
+    @Inject(method = "writeNBT", at = @At("RETURN"))
+    private void cosmiccore$saveTime(CallbackInfoReturnable<CompoundTag> cir) {
+        CompoundTag tag = cir.getReturnValue();
+        tag.putInt("badTimeTimer", cosmiccore$badTimeTimer);
+    }
+
+    @Inject(method = "readNBT", at = @At("RETURN"))
+    private void cosmiccore$loadTime(CompoundTag compound, CallbackInfo ci) {
+        if (compound.contains("badTimeTimer")) {
+            cosmiccore$badTimeTimer = compound.getInt("badTimeTimer");
+        }
     }
 }
