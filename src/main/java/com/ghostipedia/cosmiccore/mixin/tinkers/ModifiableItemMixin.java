28a6aee1e346f85b907d6b68cc96c7d8e6930e83
Kolja <104643407+Thatguyello@users.noreply.github.com>
2025-04-18 02:53:51 +0200

    Added tconstruct + mantle and start of the enchant rework.

---
+++ src/main/java/com/ghostipedia/cosmiccore/mixin/tinkers/ModifiableItemMixin.java
@@ -0,0 +1,22 @@
+package com.ghostipedia.cosmiccore.mixin.tinkers;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.item.ItemStack;
+
+import slimeknights.tconstruct.library.tools.item.ModifiableItem;
+
+@Mixin(value = ModifiableItem.class, remap = false)
+public class ModifiableItemMixin {
+    @Inject(method = "isEnchantable", at = @At("RETURN"), cancellable = true)
+    public void isEnchantable(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+    @Inject(method = "isBookEnchantable", at = @At("RETURN"), cancellable = true)
+    public void isBookEnchantable(ItemStack stack, ItemStack book, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
