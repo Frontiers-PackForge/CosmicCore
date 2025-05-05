30fd79562517dea7f14df488cc6e2caff4792f90
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-05-04 19:36:42 -0600

    Clean Up Item Renderers (#71)
    * clean up item renderers for material icon sets
    
    * merge dn

--- src/main/java/com/ghostipedia/cosmiccore/api/data/material/property/CCoreMaterialIconSet.java
+++ src/main/java/com/ghostipedia/cosmiccore/api/data/material/property/CCoreMaterialIconSet.java
@@ -1,8 +1,9 @@
 package com.ghostipedia.cosmiccore.api.data.material.property;
 
 import com.ghostipedia.cosmiccore.CosmicCore;
+import com.ghostipedia.cosmiccore.client.renderer.item.HaloItemRenderer;
 import com.ghostipedia.cosmiccore.common.data.materials.CosmicMaterialSet;
-import com.ghostipedia.cosmiccore.common.item.behavior.HaloItemBehavior;
+import com.ghostipedia.cosmiccore.utils.ColorUtil;
 
 import com.gregtechceu.gtceu.api.data.chemical.material.info.MaterialIconSet;
 import com.gregtechceu.gtceu.api.item.component.ICustomRenderer;
@@ -13,6 +14,8 @@ import net.minecraft.resources.ResourceLocation;
 
 import lombok.Getter;
 
+import java.util.function.Supplier;
+
 @Getter
 public class CCoreMaterialIconSet extends MaterialIconSet {
 
@@ -22,15 +25,23 @@ public class CCoreMaterialIconSet extends MaterialIconSet {
         this(name, parentIconset, root, renderer == null ? null : () -> renderer);
     }
 
-    private CCoreMaterialIconSet(String name, MaterialIconSet parentIconset, boolean root, ICustomRenderer renderer) {
+    public CCoreMaterialIconSet(String name, MaterialIconSet parentIconset, boolean root, ICustomRenderer renderer) {
         super(name, parentIconset, root);
         this.customRender = renderer;
     }
 
+    static final Supplier<Integer> prismaticColor = () -> {
+        float v = (float) ((System.currentTimeMillis() / 500) % 10) / 10;
+        if (v > 0.5f)
+            v = 1 - v;
+        return (0xff << 24) | ColorUtil.lerpColorRGB(0xffc0cb, 0x000080, v * 2);
+    };
+
     public static final CCoreMaterialIconSet VIBRANIUM = new CCoreMaterialIconSet("vibranium",
             CosmicMaterialSet.NEUTRONITE, false,
-            new HaloItemBehavior(5, 0xFF1c1926, new ResourceLocation(CosmicCore.MOD_ID, "rnd/halo"), true, true));
+            HaloItemRenderer.create(5, 0xFF1c1926, new ResourceLocation(CosmicCore.MOD_ID, "rnd/halo"), true, true));
 
     public static final CCoreMaterialIconSet PRISMATIC = new CCoreMaterialIconSet("prismatic", SHINY, false,
-            new HaloItemBehavior(8, 0x99FFFFFF, new ResourceLocation(CosmicCore.MOD_ID, "rnd/halo"), true, false));
+            HaloItemRenderer.create(8, prismaticColor, new ResourceLocation(CosmicCore.MOD_ID, "rnd/halo"), true,
+                    true));
 }
--- src/main/java/com/ghostipedia/cosmiccore/client/renderer/item/HaloItemRenderer.java
+++ src/main/java/com/ghostipedia/cosmiccore/client/renderer/item/HaloItemRenderer.java
@@ -1,12 +1,13 @@
 package com.ghostipedia.cosmiccore.client.renderer.item;
 
 import com.ghostipedia.cosmiccore.client.gui.AlphaOverrideVertexConsumer;
-import com.ghostipedia.cosmiccore.common.item.IHaloRender;
 
+import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.GTValues;
 
 import com.lowdragmc.lowdraglib.Platform;
 import com.lowdragmc.lowdraglib.client.model.ModelFactory;
+import com.lowdragmc.lowdraglib.client.renderer.IRenderer;
 import com.lowdragmc.lowdraglib.utils.ColorUtils;
 
 import net.minecraft.client.Minecraft;
@@ -32,15 +33,40 @@ import org.joml.Matrix4f;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import static com.ghostipedia.cosmiccore.client.renderer.utility.CosmicCoreRenderUtils.bindBlockAtlas;
 
-public class HaloItemRenderer extends WrappedItemRenderer {
+public class HaloItemRenderer extends WrappedItemRenderer implements IRenderer {
 
     private final Set<ResourceLocation> textures = new HashSet<>();
 
-    protected HaloItemRenderer() {
+    int haloSize;
+    Supplier<Integer> haloColor;
+    ResourceLocation haloTexture;
+    boolean shouldDrawHalo;
+    boolean shouldDrawPulse;
+
+    public static HaloItemRenderer create(int size, int color, ResourceLocation texture, boolean drawHalo,
+                                          boolean drawPulse) {
+        return create(size, () -> color, texture, drawHalo, drawPulse);
+    }
+
+    public static HaloItemRenderer create(int size, Supplier<Integer> color, ResourceLocation texture, boolean drawHalo,
+                                          boolean drawPulse) {
+        return GTCEu.isClientSide() ? new HaloItemRenderer(size, color, texture, drawHalo, drawPulse) : null;
+    }
+
+    private HaloItemRenderer(int size, Supplier<Integer> color, ResourceLocation texture, boolean drawHalo,
+                             boolean drawPulse) {
+        this.haloSize = size;
+        this.haloColor = color;
+        this.haloTexture = texture;
+        this.shouldDrawHalo = drawHalo;
+        this.shouldDrawPulse = drawPulse;
+
         if (Platform.isClient()) {
+            addTexture(haloTexture);
             registerEvent();
         }
     }
@@ -50,149 +76,93 @@ public class HaloItemRenderer extends WrappedItemRenderer {
         textures.add(resourceLocation);
     }
 
-    @OnlyIn(Dist.CLIENT)
     @Override
-    public void renderItem(
-                           ItemStack stack,
-                           ItemDisplayContext transformType,
-                           boolean leftHand,
-                           PoseStack poseStack,
-                           MultiBufferSource buffer,
-                           int combinedLight,
-                           int combinedOverlay,
-                           BakedModel model) {
+    @OnlyIn(Dist.CLIENT)
+    public void renderItem(ItemStack stack, ItemDisplayContext transformType, boolean leftHand, PoseStack poseStack,
+                           MultiBufferSource buffer, int combinedLight, int combinedOverlay, BakedModel model) {
         model = getVanillaModel(stack, null, null);
-        if (transformType == ItemDisplayContext.GUI &&
-                stack.getItem() instanceof CosmicCoreItemRendererProvider rendererItem) {
+        if (transformType == ItemDisplayContext.GUI) {
+            Tesselator tess = Tesselator.getInstance();
+            BufferBuilder buf = tess.getBuilder();
+            buf.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
 
-            if (rendererItem.getRenderInfo(stack) instanceof IHaloRender hri) {
-                Tesselator tess = Tesselator.getInstance();
-                BufferBuilder buf = tess.getBuilder();
-                buf.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+            poseStack.pushPose();
+            RenderSystem.enableBlend();
+            RenderSystem.disableDepthTest();
+            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA,
+                    GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
 
-                poseStack.pushPose();
-                RenderSystem.enableBlend();
-                RenderSystem.disableDepthTest();
-                RenderSystem.blendFunc(
-                        GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+            poseStack.translate(-0.5F, -0.5F, -0.5F);
 
-                poseStack.translate(-0.5F, -0.5F, -0.5F);
+            if (shouldDrawHalo) {
+                int colour = haloColor.get();
+                float r = ColorUtils.red(colour);
+                float g = ColorUtils.green(colour);
+                float b = ColorUtils.blue(colour);
+                float a = ColorUtils.alpha(colour);
 
-                if (hri.shouldDrawHalo()) {
-                    int colour = hri.haloColor();
-                    float r = ColorUtils.red(colour);
-                    float g = ColorUtils.green(colour);
-                    float b = ColorUtils.blue(colour);
-                    float a = ColorUtils.alpha(colour);
+                RenderSystem.setShaderColor(r, g, b, a);
+                RenderSystem.setShader(GameRenderer::getPositionTexShader);
+                TextureAtlasSprite sprite = ModelFactory.getBlockSprite(haloTexture);
+                bindBlockAtlas();
+                float spread = haloSize / 16F;
+                float min = 0F - spread;
+                float max = 1F + spread;
 
-                    RenderSystem.setShaderColor(r, g, b, a);
-                    RenderSystem.setShader(GameRenderer::getPositionTexShader);
-                    TextureAtlasSprite sprite = ModelFactory.getBlockSprite(hri.haloTexture());
-                    bindBlockAtlas();
-                    float spread = hri.haloSize() / 16F;
-                    float min = 0F - spread;
-                    float max = 1F + spread;
+                float minU = sprite.getU0();
+                float maxU = sprite.getU1();
+                float minV = sprite.getV0();
+                float maxV = sprite.getV1();
 
-                    float minU = sprite.getU0();
-                    float maxU = sprite.getU1();
-                    float minV = sprite.getV0();
-                    float maxV = sprite.getV1();
+                Matrix4f pos = poseStack.last().pose();
+                buf.vertex(pos, max, max, 0).uv(maxU, minV).endVertex();
+                buf.vertex(pos, min, max, 0).uv(minU, minV).endVertex();
+                buf.vertex(pos, min, min, 0).uv(minU, maxV).endVertex();
+                buf.vertex(pos, max, min, 0).uv(maxU, maxV).endVertex();
+                tess.end();
 
-                    Matrix4f pos = poseStack.last().pose();
-                    buf.vertex(pos, max, max, 0).uv(maxU, minV).endVertex();
-                    buf.vertex(pos, min, max, 0).uv(minU, minV).endVertex();
-                    buf.vertex(pos, min, min, 0).uv(minU, maxV).endVertex();
-                    buf.vertex(pos, max, min, 0).uv(maxU, maxV).endVertex();
-                    tess.end();
-
-                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-                    poseStack.popPose();
-                }
-
-                vanillaRender(
-                        stack,
-                        transformType,
-                        leftHand,
-                        poseStack,
-                        buffer,
-                        combinedLight,
-                        combinedOverlay,
-                        model);
-
-                if (hri.shouldDrawPulse()) {
-                    poseStack.pushPose();
-                    poseStack.translate(-0.5F, -0.5F, -0.5F);
-                    float scale = GTValues.RNG.nextFloat() * 0.15F + 0.95F;
-                    double trans = (1 - scale) / 2;
-                    poseStack.translate(trans, trans, 0);
-                    poseStack.scale(scale, scale, 1.0001F);
-
-                    renderAlpha(
-                            stack,
-                            transformType,
-                            leftHand,
-                            poseStack,
-                            buffer,
-                            combinedLight,
-                            combinedOverlay,
-                            model,
-                            0.6F);
-
-                    poseStack.popPose();
-                }
-                RenderSystem.enableDepthTest();
-                RenderSystem.disableBlend();
-
-            } else {
-                vanillaRender(
-                        stack,
-                        transformType,
-                        leftHand,
-                        poseStack,
-                        buffer,
-                        combinedLight,
-                        combinedOverlay,
-                        model);
+                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                poseStack.popPose();
             }
+            vanillaRender(stack, transformType, leftHand, poseStack, buffer, combinedLight, combinedOverlay, model);
 
+            if (shouldDrawPulse) {
+                poseStack.pushPose();
+                poseStack.translate(-0.5F, -0.5F, -0.5F);
+                float scale = GTValues.RNG.nextFloat() * 0.15F + 0.95F;
+                double trans = (1 - scale) / 2;
+                poseStack.translate(trans, trans, 0);
+                poseStack.scale(scale, scale, 1.0001F);
+
+                renderAlpha(stack, transformType, leftHand, poseStack, buffer, combinedLight, combinedOverlay,
+                        model, 0.6F);
+
+                poseStack.popPose();
+            }
+            RenderSystem.enableDepthTest();
+            RenderSystem.disableBlend();
         } else {
-            vanillaRender(
-                    stack, transformType, leftHand, poseStack, buffer, combinedLight, combinedOverlay, model);
+            vanillaRender(stack, transformType, leftHand, poseStack, buffer, combinedLight, combinedOverlay, model);
         }
     }
 
     @OnlyIn(Dist.CLIENT)
-    public static void renderAlpha(
-                                   ItemStack stack,
-                                   ItemDisplayContext modelTransformationMode,
-                                   boolean leftHanded,
-                                   PoseStack matrices,
-                                   MultiBufferSource buffer,
-                                   int light,
-                                   int overlay,
-                                   BakedModel model,
-                                   float alphaOverride) {
-        if (!stack.isEmpty()) {
-            model.getTransforms().getTransform(modelTransformationMode).apply(leftHanded, matrices);
-            RenderType renderType = ItemBlockRenderTypes.getRenderType(stack, true);
-            VertexConsumer vertexConsumer = ItemRenderer.getFoilBufferDirect(buffer, renderType, true, stack.hasFoil());
+    public static void renderAlpha(ItemStack stack, ItemDisplayContext modelTransformationMode, boolean leftHanded,
+                                   PoseStack matrices, MultiBufferSource buffer, int light, int overlay,
+                                   BakedModel model, float alphaOverride) {
+        if (stack.isEmpty()) return;
+        model.getTransforms().getTransform(modelTransformationMode).apply(leftHanded, matrices);
+        RenderType renderType = ItemBlockRenderTypes.getRenderType(stack, true);
+        VertexConsumer vertexConsumer = ItemRenderer.getFoilBufferDirect(buffer, renderType, true, stack.hasFoil());
 
-            Minecraft.getInstance()
-                    .getItemRenderer()
-                    .renderModelLists(
-                            model,
-                            stack,
-                            light,
-                            overlay,
-                            matrices,
-                            new AlphaOverrideVertexConsumer(vertexConsumer, alphaOverride));
-        }
+        Minecraft.getInstance().getItemRenderer()
+                .renderModelLists(model, stack, light, overlay, matrices,
+                        new AlphaOverrideVertexConsumer(vertexConsumer, alphaOverride));
     }
 
-    @OnlyIn(Dist.CLIENT)
     @Override
-    public void onPrepareTextureAtlas(
-                                      ResourceLocation atlasName, Consumer<ResourceLocation> register) {
+    @OnlyIn(Dist.CLIENT)
+    public void onPrepareTextureAtlas(ResourceLocation atlasName, Consumer<ResourceLocation> register) {
         if (atlasName.equals(InventoryMenu.BLOCK_ATLAS)) {
             textures.forEach(register);
         }
