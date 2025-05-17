f163790c374db146295b83be356aa987cf69a48e
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2025-05-17 01:28:19 -0400

    add coil base class for modular and module

---
+++ src/main/java/com/ghostipedia/cosmiccore/api/machine/multiblock/modular/CoilWorkableElectricModularMultiblockMachine.java
@@ -0,0 +1,32 @@
+package com.ghostipedia.cosmiccore.api.machine.multiblock.modular;
+
+import com.gregtechceu.gtceu.api.block.ICoilType;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.common.block.CoilBlock;
+import lombok.Getter;
+
+public class CoilWorkableElectricModularMultiblockMachine extends WorkableElectricModularMultiblockMachine {
+
+    @Getter
+    private ICoilType coilType = CoilBlock.CoilType.CUPRONICKEL;
+
+    public CoilWorkableElectricModularMultiblockMachine(IMachineBlockEntity holder, Object... args) {
+        super(holder, args);
+    }
+
+    //////////////////////////////////////
+    // *** Multiblock LifeCycle ***//
+    //////////////////////////////////////
+    @Override
+    public void onStructureFormed() {
+        super.onStructureFormed();
+        var type = getMultiblockState().getMatchContext().get("CoilType");
+        if (type instanceof ICoilType coil) {
+            this.coilType = coil;
+        }
+    }
+
+    public int getCoilTier() {
+        return coilType.getTier();
+    }
+}
---
+++ src/main/java/com/ghostipedia/cosmiccore/api/machine/multiblock/modular/CoilWorkableElectricMultiblockMachineModule.java
@@ -0,0 +1,37 @@
+package com.ghostipedia.cosmiccore.api.machine.multiblock.modular;
+
+import com.gregtechceu.gtceu.api.block.ICoilType;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.common.block.CoilBlock;
+import lombok.Getter;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class CoilWorkableElectricMultiblockMachineModule extends WorkableElectricMultiblockMachineModule {
+
+    @Getter
+    private ICoilType coilType = CoilBlock.CoilType.CUPRONICKEL;
+
+    public CoilWorkableElectricMultiblockMachineModule(IMachineBlockEntity holder, Object... args) {
+        super(holder, args);
+    }
+
+    //////////////////////////////////////
+    // *** Multiblock LifeCycle ***//
+    //////////////////////////////////////
+    @Override
+    public void onStructureFormed() {
+        super.onStructureFormed();
+        var type = getMultiblockState().getMatchContext().get("CoilType");
+        if (type instanceof ICoilType coil) {
+            this.coilType = coil;
+        }
+    }
+
+    public int getCoilTier() {
+        return coilType.getTier();
+    }
+}
