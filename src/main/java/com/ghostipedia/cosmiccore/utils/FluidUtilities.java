c239f99a41af32d2f4d7979af9e02bc2857907a7
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-07-31 11:46:40 -0600

    new utils for getting fluid sprites (both still and flowing)

--- src/main/java/com/ghostipedia/cosmiccore/utils/FluidUtilities.java
+++ src/main/java/com/ghostipedia/cosmiccore/utils/FluidUtilities.java
@@ -1,8 +1,13 @@
 package com.ghostipedia.cosmiccore.utils;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class FluidUtilities {
@@ -11,4 +16,19 @@ public class FluidUtilities {
         Fluid fluid = ForgeRegistries.FLUIDS.getValue(new ResourceLocation(fluidResloc));
         return fluid == null ? Fluids.EMPTY : fluid;
     }
+
+    @SuppressWarnings("deprecation")
+    public static TextureAtlasSprite getStillSprite(Fluid fluid) {
+        FluidStack fluidStack = new FluidStack(fluid, 1);
+        return  Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS)
+                .apply(IClientFluidTypeExtensions.of(fluid).getStillTexture(fluidStack));
+    }
+
+    @SuppressWarnings("deprecation")
+    public static TextureAtlasSprite getFlowingSprite(Fluid fluid) {
+        FluidStack fluidStack = new FluidStack(fluid, 1);
+        return  Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS)
+                .apply(IClientFluidTypeExtensions.of(fluid).getFlowingTexture(fluidStack));
+    }
+
 }
