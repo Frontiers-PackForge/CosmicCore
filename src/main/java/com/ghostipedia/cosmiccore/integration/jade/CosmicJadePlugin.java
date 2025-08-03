10dbad58308fda4855bffb6bb763e67ac606669e
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2025-08-02 22:00:51 -0600

    jade plugin

---
+++ src/main/java/com/ghostipedia/cosmiccore/integration/jade/CosmicJadePlugin.java
@@ -0,0 +1,22 @@
+package com.ghostipedia.cosmiccore.integration.jade;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaCommonRegistration;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
+
+@WailaPlugin
+public class CosmicJadePlugin implements IWailaPlugin {
+
+    @Override
+    public void register(IWailaCommonRegistration registration) {
+        registration.registerBlockDataProvider(new MultiLogicProvider(), BlockEntity.class);
+    }
+
+    @Override
+    public void registerClient(IWailaClientRegistration registration) {
+        registration.registerBlockComponent(new MultiLogicProvider(), Block.class);
+    }
+}
