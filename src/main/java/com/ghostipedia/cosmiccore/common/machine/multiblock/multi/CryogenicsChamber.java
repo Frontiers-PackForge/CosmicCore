52af3906b31284e477ab054384da8133972eeb4c
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-09-14 15:53:01 -0600

    cryo chamber ctm fix (#135)
    * fixed cry chamber fuckery
    
    * SPOTLESS

--- src/main/java/com/ghostipedia/cosmiccore/common/machine/multiblock/multi/CryogenicsChamber.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/machine/multiblock/multi/CryogenicsChamber.java
@@ -1,7 +1,6 @@
 package com.ghostipedia.cosmiccore.common.machine.multiblock.multi;
 
 import com.ghostipedia.cosmiccore.CosmicCore;
-import com.ghostipedia.cosmiccore.client.renderer.machine.CosmicDynamicRenderHelpers;
 import com.ghostipedia.cosmiccore.gtbridge.CosmicRecipeTypes;
 
 import com.gregtechceu.gtceu.GTCEu;
@@ -29,6 +28,8 @@ public class CryogenicsChamber {
             .rotationState(RotationState.NON_Y_AXIS)
             .recipeType(CosmicRecipeTypes.CRYOGENICS_CHAMBER)
             .hasBER(true)
+            .partAppearance((controller, part, side) -> CRYOGENIC_CASING.getDefaultState())
+            .appearanceBlock(HEAVY_FROST_PROOF_CASING)
             .recipeModifiers(GTRecipeModifiers.PARALLEL_HATCH,
                     GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK_SUBTICK),
                     GTRecipeModifiers.BATCH_MODE)
@@ -71,9 +72,7 @@ public class CryogenicsChamber {
             .model(createSeparateControllerCasingMachineModel(
                     CosmicCore.id("block/casings/solid/heavy_frost_proof_casing"),
                     CosmicCore.id("block/casings/solid/cryogenic_casing"),
-                    GTCEu.id("block/multiblock/hpca")).andThen(
-                            model -> model
-                                    .addDynamicRenderer(CosmicDynamicRenderHelpers::createCryoChamberPartRender)))
+                    GTCEu.id("block/multiblock/hpca")))
             .register();
 
     public static void init() {}
