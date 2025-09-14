a5b31efe8f68fcc6ef62f2e0c324d80b16563a77
Karthi <75553966+JuiceyBeans@users.noreply.github.com>
2025-09-14 22:28:27 +0100

    Fix LSO mixin remaps (#134)

--- src/main/java/com/ghostipedia/cosmiccore/mixin/lso/HeatstrokeEffectMixin.java
+++ src/main/java/com/ghostipedia/cosmiccore/mixin/lso/HeatstrokeEffectMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import sfiomn.legendarysurvivaloverhaul.common.effects.HeatStrokeEffect;
 
-@Mixin(value = HeatStrokeEffect.class)
+@Mixin(value = HeatStrokeEffect.class, remap = false)
 public abstract class HeatstrokeEffectMixin extends MobEffect {
 
     protected HeatstrokeEffectMixin(MobEffectCategory category, int color) {
