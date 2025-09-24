61c1e767645faf8a12f0ecfa051a8befa93eadb8
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2025-09-23 23:53:14 -0600

    step one of tinkers material builder system //todo EVERYTHING THAT I DIDNT DO

---
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/datagen/CosmicMaterialTraits.java
@@ -0,0 +1,72 @@
+package com.ghostipedia.cosmiccore.common.data.datagen;
+
+import com.ghostipedia.cosmiccore.CosmicCore;
+import com.ghostipedia.cosmiccore.common.data.materials.tinkers.TinkersMaterial;
+import com.ghostipedia.cosmiccore.common.data.materials.tinkers.TinkersMaterial.Builder;
+import com.ghostipedia.cosmiccore.common.item.tcon.modifiers.CosmicCoreModifiers;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+
+import slimeknights.tconstruct.library.data.material.AbstractMaterialDataProvider;
+import slimeknights.tconstruct.library.data.material.AbstractMaterialTraitDataProvider;
+import slimeknights.tconstruct.library.materials.MaterialRegistry;
+import slimeknights.tconstruct.library.materials.stats.MaterialStatsId;
+import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.util.LazyModifier;
+import slimeknights.tconstruct.tools.TinkerModifiers;
+import slimeknights.tconstruct.tools.data.ModifierIds;
+import slimeknights.tconstruct.tools.data.material.MaterialIds;
+import slimeknights.tconstruct.tools.modifiers.slotless.OverslimeModifier;
+import slimeknights.tconstruct.tools.stats.StatlessMaterialStats;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CosmicMaterialTraits extends AbstractMaterialTraitDataProvider {
+
+    public CosmicMaterialTraits(PackOutput packOutput, AbstractMaterialDataProvider materials) {
+        super(packOutput, materials);
+    }
+
+    public static final TinkersMaterial TEST_MATERIAL = new TinkersMaterial.Builder("shiboubouya_dn_tube")
+            .materialValue(2)
+            .headMaterialStats(2200, 10.0f, Tiers.IRON, 5.0f)
+            .gripMaterialStats(3000, 10.0f, 20)
+            .handleMaterialStats(3000, 10, 4, 10)
+            .addStatlessType(StatlessMaterialStats.BINDING)
+            .addStatlessType(StatlessMaterialStats.REPAIR_KIT)
+            .addStatlessType(StatlessMaterialStats.BOWSTRING)
+            .defaultTrait(CosmicCoreModifiers.wrenchModeSwitch)
+            .trait(CosmicCoreModifiers.wrenchModeSwitch, 3, MaterialRegistry.RANGED)
+            .sortOrder(10)
+            .build();
+
+    @Override
+    protected void addMaterialTraits() {
+        for(TinkersMaterial material : TinkersMaterial.MATERIALS) {
+                Set<LazyModifier> defaultTraits = material.getDefaultTraits();
+                if(!defaultTraits.isEmpty()){
+                addDefaultTraits(material.getMaterialName(), material.getDefaultTraits().toArray(defaultTraits.toArray(new LazyModifier[0])));
+                }
+            Map<MaterialStatsId, Set<ModifierEntry>> statTraits = material.getTraits();
+            if (!statTraits.isEmpty()) {
+                for (Map.Entry<MaterialStatsId, Set<ModifierEntry>> entry : statTraits.entrySet()) {
+                    addTraits(
+                            material.getMaterialName(),
+                            entry.getKey(),
+                            entry.getValue().toArray(new ModifierEntry[0]));
+                }
+            }
+
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
---
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/materials/tinkers/TinkersMaterial.java
@@ -0,0 +1,131 @@
+package com.ghostipedia.cosmiccore.common.data.materials.tinkers;
+
+import net.minecraft.world.item.Tier;
+
+import lombok.Builder;
+import lombok.Getter;
+import slimeknights.tconstruct.library.materials.definition.MaterialId;
+import slimeknights.tconstruct.library.materials.stats.MaterialStatsId;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.util.LazyModifier;
+import slimeknights.tconstruct.library.tools.stat.ToolStats;
+import slimeknights.tconstruct.tools.stats.*;
+
+import java.util.*;
+
+@Getter
+public class TinkersMaterial {
+
+    public static final Set<TinkersMaterial> MATERIALS = new HashSet<>();
+    private String name;
+    private Set<LazyModifier> defaultTraits = new HashSet<>();
+    private Map<MaterialStatsId, Set<ModifierEntry>> traits = new HashMap<>();
+    private int materialValue;
+    private HeadMaterialStats headMaterialStats;
+    private HandleMaterialStats handleMaterialStats;
+    private GripMaterialStats gripMaterialStats;
+    private Set<StatlessMaterialStats> statlessMaterialStats = new HashSet<>();
+    private SkullStats skullStats;
+    private ToolStats toolStats;
+    private int sortOrder;
+
+    public MaterialId getMaterialName() {
+        return new MaterialId("cosmiccore", this.name);
+    }
+
+    private TinkersMaterial(Builder builder) {
+        this.name = builder.name;
+        this.materialValue = builder.materialValue;
+        this.headMaterialStats = builder.headMaterialStats;
+    }
+
+    public static final class Builder {
+
+        private String name;
+        private Set<LazyModifier> modifiers = new HashSet<>();
+        private int materialValue = 1;
+        private int sortOrder = 0; // Default to general
+        private HeadMaterialStats headMaterialStats;
+        private HandleMaterialStats handleMaterialStats;
+        private GripMaterialStats gripMaterialStats;
+        private Set<StatlessMaterialStats> statlessMaterialStats = new HashSet<>();
+        private SkullStats skullStats;
+        private ToolStats toolStats;
+        private Set<LazyModifier> defaultTraits;
+        private Map<MaterialStatsId, List<ModifierEntry>> statSpecificTraits = new HashMap<>();
+
+        public Builder(String name) {
+            this.name = name;
+        }
+
+        public Builder modifier(LazyModifier modifier) {
+            modifiers.add(modifier);
+            return this;
+        }
+
+        public Builder materialValue(int materialValue) {
+            this.materialValue = materialValue;
+            return this;
+        }
+
+        public Builder sortOrder(int sortOrder) {
+            this.sortOrder = sortOrder;
+            return this;
+        }
+
+        public Builder headMaterialStats(int durability, float miningSpeed, Tier miningLevel, float attack) {
+            this.headMaterialStats = new HeadMaterialStats(durability, miningSpeed, miningLevel, attack);
+            return this;
+        }
+
+        public Builder handleMaterialStats(float durability, float attackDamage, float attackSpeed, float miningSpeed) {
+            this.handleMaterialStats = new HandleMaterialStats(durability, attackDamage, attackSpeed, miningSpeed);
+            return this;
+        }
+
+        public Builder gripMaterialStats(float durability, float attackSpeed, float meleeAttack) {
+            this.gripMaterialStats = new GripMaterialStats(durability, attackSpeed, meleeAttack);
+            return this;
+        }
+
+        public Builder addStatlessType(StatlessMaterialStats type) {
+            this.statlessMaterialStats.add(type);
+            return this;
+        }
+
+        public Builder toolStats(ToolStats toolStats) {
+            this.toolStats = toolStats;
+            return this;
+        }
+
+        /**
+         * Adds a simple, level 1 trait. Corresponds to addDefaultTraits().
+         *
+         * @param modifier The modifier to add as a default trait.
+         * @return The builder instance.
+         */
+        public Builder defaultTrait(LazyModifier modifier) {
+            this.defaultTraits.add(modifier);
+            return this;
+        }
+
+        /**
+         * Adds a trait with a specific level. Corresponds to addTraits().
+         *
+         * @param modifier The modifier to add as a leveled trait.
+         * @param level    The level of the modifier.
+         * @return The builder instance.
+         */
+        public Builder trait(LazyModifier modifier, int level, MaterialStatsId statsId) {
+            List<ModifierEntry> traits = this.statSpecificTraits.computeIfAbsent(statsId, k -> new ArrayList<>());
+            traits.add(new ModifierEntry(modifier.get(), level));
+            return this;
+        }
+
+        public TinkersMaterial build() {
+            TinkersMaterial material = new TinkersMaterial(this);
+            TinkersMaterial.MATERIALS.add(material);
+            return material;
+        }
+    }
+}
