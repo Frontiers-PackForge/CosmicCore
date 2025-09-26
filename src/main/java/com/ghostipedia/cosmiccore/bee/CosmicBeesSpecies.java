7e99a3d01840e779afaa0595ea7af1696d29edc4
Ghostipedia <Ghostipedia@gmail.com>
2025-09-26 09:06:37 -0400

    New bees

--- src/main/java/com/ghostipedia/cosmiccore/bee/CosmicBeesSpecies.java
+++ src/main/java/com/ghostipedia/cosmiccore/bee/CosmicBeesSpecies.java
@@ -6,8 +6,6 @@ import net.minecraft.resources.ResourceLocation;
 
 public class CosmicBeesSpecies {
 
-    public static final ResourceLocation COBBLED = CosmicCore.id("bee_cobbled");
-
     public static final ResourceLocation LOFTY_OXYGEN = CosmicCore.id("bee_oxygen");
     public static final ResourceLocation LOFTY_HYDROGEN = CosmicCore.id("bee_hydrogen");
     public static final ResourceLocation LOFTY_NITROGEN = CosmicCore.id("bee_nitrogen");
@@ -22,4 +20,13 @@ public class CosmicBeesSpecies {
     public static final ResourceLocation SOUL = CosmicCore.id("bee_soul");
     public static final ResourceLocation RUNIC = CosmicCore.id("bee_runic");
     public static final ResourceLocation AMBROSIC = CosmicCore.id("bee_ambrosic");
+
+    public static final ResourceLocation ABRASIVE = CosmicCore.id("bee_abrasive");
+    public static final ResourceLocation ENERGIZED = CosmicCore.id("bee_energized");
+    public static final ResourceLocation SLICK = CosmicCore.id("bee_slick");
+    public static final ResourceLocation PYROLYTIC = CosmicCore.id("bee_pyrolytic");
+
+    public static final ResourceLocation LUNAR = CosmicCore.id("bee_lunar");
+    public static final ResourceLocation SOLAR = CosmicCore.id("bee_solar");
+    public static final ResourceLocation COSMOS = CosmicCore.id("bee_cosmos");
 }
