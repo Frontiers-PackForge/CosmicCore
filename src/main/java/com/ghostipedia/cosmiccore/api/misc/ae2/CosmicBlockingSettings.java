803e89d268c1fd469cfd9b98f4c161674a1ebc2e
Kolja <104643407+Thatguyello@users.noreply.github.com>
2025-09-29 01:52:06 +0200

    revert blocking mode, as it is now in ae2cl

--- src/main/java/com/ghostipedia/cosmiccore/api/misc/ae2/CosmicBlockingSettings.java
+++
@@ -1,8 +0,0 @@
-package com.ghostipedia.cosmiccore.api.misc.ae2;
-
-import appeng.api.config.Setting;
-
-public final class CosmicBlockingSettings {
-
-    public static Setting<BlockingMode> BLOCKING_MODE;
-}
