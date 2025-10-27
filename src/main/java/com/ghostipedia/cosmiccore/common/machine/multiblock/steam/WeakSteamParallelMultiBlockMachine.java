0fbff2677f6fea2bdc7b917d89a6d9ee645842fd
DilithiumThoride <lilithiumathena@gmail.com>
2025-10-26 23:18:00 -0500

    %t spotless

--- src/main/java/com/ghostipedia/cosmiccore/common/machine/multiblock/steam/WeakSteamParallelMultiBlockMachine.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/machine/multiblock/steam/WeakSteamParallelMultiBlockMachine.java
@@ -3,12 +3,12 @@ package com.ghostipedia.cosmiccore.common.machine.multiblock.steam;
 import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
-import com.gregtechceu.gtceu.common.machine.multiblock.steam.SteamParallelMultiblockMachine;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.RecipeHelper;
 import com.gregtechceu.gtceu.api.recipe.content.ContentModifier;
 import com.gregtechceu.gtceu.api.recipe.modifier.ModifierFunction;
 import com.gregtechceu.gtceu.api.recipe.modifier.ParallelLogic;
+import com.gregtechceu.gtceu.common.machine.multiblock.steam.SteamParallelMultiblockMachine;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -29,9 +29,9 @@ public class WeakSteamParallelMultiBlockMachine extends SteamParallelMultiblockM
 
     public static ModifierFunction recipeModifier(@NotNull MetaMachine machine, @NotNull GTRecipe recipe) {
         if (RecipeHelper.getRecipeEUtTier(recipe) > GTValues.LV) return ModifierFunction.NULL;
-//        long euTick = RecipeHelper.getRecipeEUtTier(recipe);
+        // long euTick = RecipeHelper.getRecipeEUtTier(recipe);
         int parallel = ParallelLogic.getParallelAmount(machine, recipe, 4);
-//         double eutMulti = (euTick * 0.5 * parallel <= 32) ? (parallel * 0.5) : (32.0 / euTick);
+        // double eutMulti = (euTick * 0.5 * parallel <= 32) ? (parallel * 0.5) : (32.0 / euTick);
 
         return ModifierFunction.builder()
                 .inputModifier(ContentModifier.multiplier(parallel))
