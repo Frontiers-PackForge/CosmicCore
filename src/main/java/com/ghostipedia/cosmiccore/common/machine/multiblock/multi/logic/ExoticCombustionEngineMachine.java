c996a0037efeadaf06c41efcee00df4240483005
CatPlasma <110224489+CatiPlasma@users.noreply.github.com>
2025-12-13 01:16:23 +0800

    Fix combustion intakes on Y axis (#179)
    Fix issue that combustion intakes always being obstructed when main body faced Y axis.

--- src/main/java/com/ghostipedia/cosmiccore/common/machine/multiblock/multi/logic/ExoticCombustionEngineMachine.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/machine/multiblock/multi/logic/ExoticCombustionEngineMachine.java
@@ -87,13 +87,17 @@ public class ExoticCombustionEngineMachine extends WorkableElectricMultiblockMac
 
     private boolean isIntakesObstructed() {
         var dir = this.getFrontFacing();
-        boolean mutableXZ = dir.getAxis() == Direction.Axis.Z;
+        var axis = dir.getAxis();
         var centerPos = this.getPos().relative(dir);
         for (int x = -1; x < 2; x++) {
             for (int y = -1; y < 2; y++) {
                 if (x == 0 && y == 0)
                     continue;
-                var blockPos = centerPos.offset(mutableXZ ? x : 0, y, mutableXZ ? 0 : x);
+                var blockPos = switch (axis) {
+                    case X -> centerPos.offset(0, x, y);
+                    case Y -> centerPos.offset(x, 0, y);
+                    case Z -> centerPos.offset(x, y, 0);
+                };
                 var blockState = this.getLevel().getBlockState(blockPos);
                 if (!blockState.isAir())
                     return true;
