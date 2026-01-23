b04242e234bdaa4dba1ea22b4048b88a5b53ab29
Spicierspace153 <21964509+Spicierspace153@users.noreply.github.com>
2026-01-23 11:11:15 -0700

    tinkers materials but cool and based and builder pilled :pepechill: (#146)
    * step one of tinkers material builder system
    //todo EVERYTHING THAT I DIDNT DO
    
    * spotless this thing
    
    * start CosmicTinkersMaterials.java
    
    * dynamically generate tool stats now
    
    * oops no errors
    
    * HELP!!!!!
    
    * FUCK THIS FUCKASS GAME HOLY SHIT
    
    * test datagen PLEASE REMMBER TO DELETE
    
    * lets just make it clean so i can cry
    
    * Fix datagen
    
    * Add noTrait so material at least gets registered
    
    * i really hope i don't have to do too much more for color
    
    * //todo: cleanup? i think this is jsut about done holy shit im free
    
    * tinkersSpotless
    
    * BETTER MATERIAL
    
    * every tool prop now has tinkers.
    todo/ make this not shit (also lang)
    
    ---------
    
    Co-authored-by: jurrejelle <jurre@jilles.com>

--- src/main/java/com/ghostipedia/cosmiccore/common/data/datagen/CosmicDataGenerators.java
+++ src/main/java/com/ghostipedia/cosmiccore/common/data/datagen/CosmicDataGenerators.java
@@ -22,7 +22,6 @@ public class CosmicDataGenerators {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-        var registries = event.getLookupProvider();
 
         boolean server = event.includeServer();
         generator.addProvider(server, new CosmicModifierProvider(packOutput));
